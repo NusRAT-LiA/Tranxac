403309bca0cbe30e82b0fdc32403da2cad5a3bf1
NusRAT-Lia <nusratlia424@gmail.com>
2022-12-15 01:06:02 +0600

    Primary structure given

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { Navbar, Welcome,Loader , Transactions,Services ,Footer } from "./components";
+
+const App=()=> {
+
+
+  return (
+    <div className="App">
+          <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
+
+export default App
