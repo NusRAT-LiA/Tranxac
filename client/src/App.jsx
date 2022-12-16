31f2c6419150b6f18c48c9666c45f6b766b2d25a
NusRAT-Lia <nusratlia424@gmail.com>
2022-12-17 01:29:24 +0600

    Nav Complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,18 @@ const App=()=> {
 
 
   return (
-    <div className="App">
-          <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="min-h-screen">
+      <div className="gradient-bg-header">
+        <Navbar/>
+        <Welcome/>
+        </div>
+        <Services/>
+        <Transactions/>
+        <Footer/>
+         
     </div>
   )
 }
+
 
 export default App
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,54 @@
+import { HiMenu } from 'react-icons/hi';
+import {AiOutlineCloseSquare } from 'react-icons/ai';
+import { useState } from 'react';
+
+import logo from '../../images/logo.png';
+
+const NavBarItem = ({title , classProp})=>{
+    return(
+        <li className="mx-6 cursor-pointer ${classProp}">
+            {title}
+        </li>
+    );
+}
+
 const Navbar =()=>
 {
+
+   const [toggleMenu, setToggleMenu] = useState(false)
     return(
-        <h1>Navbar</h1>
+        <nav className="w-full flex md:justify-center justify-between items-center p-4">
+           <div className="md:flex-[0.5] flex-initial justify-center items-center">
+           <img src={logo} alt="logo" className="w-32 cursor-pointer" />
+           </div>
+           <div>
+            <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
+             {["Wallet","Transactions","Contact","Assets"].map((item)=>(<NavBarItem key={item} title={item}/>))}
+             <li className="bg-[beige] text-black px-8 py-3 mx-6 rounded-full hover:bg-[silver] cursor-pointer">
+                Login
+             </li>
+            </ul>
+              <div className="flex relative">
+                {
+                    toggleMenu
+                    ?<AiOutlineCloseSquare fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(false)}/>
+                    :<HiMenu fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(true)}/>
+                }
+                {toggleMenu && (
+          <ul className="z-10 fixed -top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
+          flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in">
+            
+            <li className="text-xl w-full my-2"><AiOutlineCloseSquare onClick={() => setToggleMenu(false)} /></li>
+            {["Wallet","Transactions","Contact","Assets"].map(
+              (item) => <NavBarItem key={item} title={item} classprop="my-2 text-lg" />,
+            )}
+          </ul>
+        )}
+
+              </div>
+
+           </div>
+        </nav>
     );
 }
 
